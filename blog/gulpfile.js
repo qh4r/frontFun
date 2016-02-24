1336f67f2fb499ac0713e58df675a49f38f6f83a
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-02-24 21:16:39 +0100

    Started blog and setup livereload with gulp

---
+++ blog/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload');
+
+gulp.task('default', function(){
+    livereload.listen();
+    gulp.watch('./*.html', ['reload']);
+    gulp.watch('./*.css', ['reload']);
+})
+
+gulp.task('reload',function(){
+    gulp.src('./*.html').pipe(livereload());
+    livereload('./*.css').pipe(livereload());
+})
