bbabcc0ddc8bf9420da694f5eb75514c5db38255
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-03-12 19:55:51 +0100

    Added auto prefix and minification of css to gulpfile

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp'),
     livereload = require('gulp-livereload');
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var csswring = require('csswring');
 
-gulp.task('default', function(){
+gulp.task('default', function () {
     livereload.listen();
     gulp.watch('./*.html', ['reload']);
-    gulp.watch('./resources/css/*.css', ['reload']),
+    gulp.watch('./resources/work_css/*.css', ['css']);
+    gulp.watch('./resources/css/*.css', ['reload']);
     gulp.watch('./resources/js/*.js', ['reload']);
 })
 
-gulp.task('reload',function(){
+gulp.task('reload', function () {
     gulp.src('./*.html').pipe(livereload());
     //gulp.src('./*.css').pipe(livereload());
 })
+
+gulp.task('css', function () {
+    var processors = [
+        autoprefixer({browsers: ['last 3 version']}),
+        mqpacker,
+        csswring
+    ];
+    return gulp.src('./resources/work_css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./resources/css/'));
+});
