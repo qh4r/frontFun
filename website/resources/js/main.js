c0bb0ba8e916c2a6fa83fe589eac6366efc140ff
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-03-13 14:36:17 +0100

    some jquery font fun

--- website/resources/js/main.js
+++ website/resources/js/main.js
@@ -1,8 +1,18 @@
 $(function(){
-   $('h2').on('click', function(){
+    var changeFontSize = function(element, difference) {
+        //ucina tekst od 0 do 2 od konca
+        // to usuwa dopisek px
+        //przeksztalca na number i dodaje 2
+        // dodaje px i zapisuje nowa wartosc
+        element.css('font-size', Math.abs(((Number(element.css('font-size').slice(0,-2))
+            + Number(difference) || 0 ) || 1))+'px');
+    };
+    //click przechwytuje tylko left click
+   $('h2').on('mousedown', function(event){
        var thisH2 = $(this);
-       console.log('before: ', thisH2.css('background-color'));
-       thisH2.css('background-color', '#ff0000');
-       console.log('after: ', thisH2.css('background-color'));
+       // 1 - lewy, 2 - srodkowy, 3 - prawy
+       console.log(event.which);
+       changeFontSize(thisH2, event.which < 2 ? 3 : -3);
+       console.log(thisH2.css('font-size'));
    });
 });
