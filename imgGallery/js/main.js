c82d83d491139bde8bffdf39dda1086d57c4a6b9
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-04-09 00:31:10 +0200

    Started image gallery project

---
+++ imgGallery/js/main.js
@@ -0,0 +1,7 @@
+define(['domReady', 'jquery'],function(domReady){
+    domReady(function(){
+        //console.log(document.getElementsByTagName('html')[0].innerHTML);
+        console.log("dziala");
+        //$('body').prepend('<h1>dziala</h1>');
+    });
+});
