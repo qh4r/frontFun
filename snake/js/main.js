2f48128d0a4d5b3a343b776c865f8a3ea6b2d0b6
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-04-14 20:15:25 +0200

    Added styles and model for snake

--- snake/js/main.js
+++ snake/js/main.js
@@ -1,6 +1,53 @@
-define(['domReady'],function(domReady){
-    domReady(function(){
+define(['domReady', 'jquery'], function (domReady) {
+    domReady(function () {
         //console.log(document.getElementsByTagName('html')[0].innerHTML);
-        console.log("dziala");
+
+        var gameboard = (function () {
+            var canvas = $('#canvas')[0],
+                context = this.canvas.getContext('2d'),
+                width = this.canvas.width,
+                direction = 'right',
+                height = this.canvas.height,
+                cellWidth = 15,
+                speed = 150,
+                snakeArray = [];
+
+            function createSnake() {
+                var length = 4;
+                for (var i = length - 1; i > -1; i--)
+                    snakeArray.push({x: i, y: 0});
+            };
+
+            function createFood() {
+                var foot = {
+                    x: Math.round(Math.random((width - cellWidth) / cellWidth)),
+                    y: Math.round(Math.random((height - cellWidth) / cellWidth))
+                }
+            };
+
+            function getSpeed() {
+                return speed;
+            }
+
+            function paint() {
+
+            }
+
+            return {
+                getSpeed: getSpeed,
+                createSnake: createSnake,
+                createFood: createFood,
+                paint: paint
+            }
+        })();
+
+        (function init() {
+            gameboard.createSnake();
+            gameboard.createFood();
+            gameboard.score = 0;
+
+            if (typeof game_loop != "undefined") clearInterval(game_loop);
+            var gameLoop = setInterval(gameboard.paint, gameboard.getSpeed());
+        })();
     });
 });
