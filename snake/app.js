8c380e0a35b00353b3fcdfd901403a17d49316a6
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-04-14 18:53:10 +0200

    Started snake game project

---
+++ snake/app.js
@@ -0,0 +1,10 @@
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        app: '../js_min',
+        domReady: 'domReady'
+        //jquery: 'https://code.jquery.com/jquery-2.2.2.min'
+    }
+});
+
+requirejs(['app/main']);
