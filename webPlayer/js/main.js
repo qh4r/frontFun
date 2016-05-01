16c17a53da22a00e2408072d372811aa9db0f8f7
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-05-01 15:03:37 +0200

    Cleaned up webplayer

--- webPlayer/js/main.js
+++ webPlayer/js/main.js
@@ -1,36 +1,14 @@
 define(['domReady', 'jquery'], function (domReady) {
     domReady(function () {
-        var playButton = $('#play');
-        var pauseButton = $('#pause');
-        var stopButton = $('#stop');
-        var volumeBar = $('#volume');
-        var duration = $('#duration');
-        var progress = $('#progress');
-        var audio = new Audio('media/Canon.mp3');
-        playButton.on('click', function () {
-            audio.play();
-            togglePlayerState(playButton, pauseButton);
-        });
-
-        pauseButton.on('click', function () {
-            audio.pause();
-            togglePlayerState(playButton, pauseButton);
-        });
-
-        stopButton.on('click', function () {
-            audio.pause();
-            audio.currentTime = 0;
-            goToStopState(playButton, pauseButton);
-        });
-
-        volumeBar.on('change', function () {
-            console.log('change', this.value);
-            audio.volume = parseFloat(this.value / 40);
-        });
-        showDuration(audio, duration, progress);
-
-        //console.log(document.getElementsByTagName('html')[0].innerHTML);
-        console.log("dziala");
+        var controls = {
+            playButton: $('#play'),
+            pauseButton: $('#pause'),
+            stopButton: $('#stop'),
+            volumeBar: $('#volume'),
+            duration: $('#duration'),
+            progress: $('#progress')
+        };
+        initializeAudio(controls);
     });
 
     function togglePlayerState(play, pause) {
@@ -49,13 +27,48 @@ define(['domReady', 'jquery'], function (domReady) {
         audio.addEventListener('timeupdate', function (time) {
             var s = parseInt(audio.currentTime % 60);
             var m = parseInt(audio.currentTime / 60) % 60;
-            s = s < 10 ? "0"+s : s;
+            s = s < 10 ? "0" + s : s;
             duration.text(m + ':' + s);
 
-            if(audio.currentTime > 0){
+            if (audio.currentTime > 0) {
                 value = Math.floor((100 / audio.duration) * audio.currentTime);
             }
-            progress.css('width', value+'%');
+            progress.css('width', value + '%');
         });
+    }
+
+    function setNewAudio(element) {
+        var song= element.attr && element.attr('song');
+        var title= element.text && element.text();
+        var audio = new Audio('media/sch_theme.mp3');
+        return audio;
+    }
+
+    function initializeAudio(controls) {
+
+        var audio = setNewAudio({});
+
+        controls.playButton.on('click', function () {
+            audio.play();
+            togglePlayerState(controls.playButton, controls.pauseButton);
+        });
+
+        controls.pauseButton.on('click', function () {
+            audio.pause();
+            togglePlayerState(controls.playButton, controls.pauseButton);
+        });
+
+        controls.stopButton.on('click', function () {
+            audio.pause();
+            audio.currentTime = 0;
+            goToStopState(controls.playButton, controls.pauseButton);
+        });
+
+        controls.volumeBar.on('change', function () {
+            audio.volume = parseFloat(this.value / 40);
+        });
+        showDuration(audio, controls.duration, controls.progress);
+
+        console.log("dziala");
     }
 });
